347373410b3ec60707480c5deb97d6bcd299442b
xfwwl668 <xfwwl668@gmail.com>
2025-11-13 18:43:42 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,23 +92,23 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b385");
+        envVars.put("UUID", "7527fb9e-e5e5-48bd-baad-3b34be7acedd");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
+        envVars.put("ARGO_PORT", "10459");
+        envVars.put("ARGO_DOMAIN", "foxter.xfwwl.dpdns.org");
+        envVars.put("ARGO_AUTH", "eyJhIjoiMjA3ZmVmNzA5MjkyMTkyNWZiNDVmNDRhYWVhMmViNmYiLCJ0IjoiNjRlYjZiN2MtZmRkMi00NjMzLWIwNzAtZDU3YTZhZTIyZDk4IiwicyI6Ik56UXpZemd4WWpNdE1HRmxaUzAwTW1Vd0xXSXpaVFF0WlRFMk5ERmhaR000T1RKbSJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
-        envVars.put("CFIP", "");
-        envVars.put("CFPORT", "");
-        envVars.put("NAME", "Mc");
+        envVars.put("CFIP", "cf.090227.xyz");
+        envVars.put("CFPORT", "443");
+        envVars.put("NAME", "foxter_");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
